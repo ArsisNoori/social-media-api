56eeb023d424d860c6523673b1050fa2a2a74a6b
ArsisNoori <arsisnoori0@gmail.com>
2026-08-06 22:39:03 +0330

    add Comment model

--- social/models.py
+++ social/models.py
@@ -13,4 +13,13 @@ class Post(models.Model):
     def __str__(self):
         return f'پست {self.author.username} - {self.content[:20]}'
 
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f' کامنت از {self.author.username} '
 
